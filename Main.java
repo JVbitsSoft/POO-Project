4d11e16f7568af828dd35bdc674cf8862d181eb4
JVbitsSoft <63216453+JVbitsSoft@users.noreply.github.com>
2022-11-15 23:34:11 -0300

    Enviando de novo.

--- Main.java
+++ Main.java
@@ -1,15 +1,25 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Defina uma porta para o Servidor: ");
         String port = scanner.nextLine();
         if (isNumeric(port)) {
             Server server = new Server(Integer.parseInt(port));
             server.openConnection();
         } else {
-            System.out.println("Invalid value");
+            System.out.println("O valor da porta está invalido");
+        }
+
+        System.out.print("Pressione a tecla Enter para fechar...");
+        try {
+            System.in.read();
+            scanner.nextLine();
+        } catch (IOException e) {
+            // e.printStackTrace();
         }
         scanner.close();
     }
